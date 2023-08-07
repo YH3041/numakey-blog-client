20c338b7f171dedb423359726092e403b410ed5b
YH3041 <mmj2rv9@gmail.com>
2023-08-07 11:02:09 +0900

    fix : 프로덕션 next-auth 500 이슈 해결

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+const dotenv = require('dotenv');
+
+if (process.env.NODE_ENV == 'production') {
+    isProd = true;
+    dotenv.config({ path: path.join(process.env.PWD, 'envs', '.env.production') });
+} else if (process.env.NODE_ENV == 'stage') {
+    dotenv.config({ path: path.join(process.env.PWD, 'envs', '.env.stage') });
+} else {
+    dotenv.config({ path: path.join(process.env.PWD, 'envs', '.env.development') });
+}
+
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
